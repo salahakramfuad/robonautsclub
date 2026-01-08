b2694c673317d5c35b19e87822375267921384b1
salahakramfuad <salahakramfuad@gmail.com>
2026-01-08 19:35:32 +0600

    Add Cloudinary support and update CORS settings in next.config.ts
    - Added Cloudinary and Firebase storage hostnames to CORS configuration in next.config.ts for improved media handling.
    - Updated package.json to include next-cloudinary dependency for enhanced image management.
    - Updated pnpm-lock.yaml to reflect new dependencies and their versions.

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
